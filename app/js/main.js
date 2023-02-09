1b4816503e32c43166c881fc593073dc4b7d8d63
Kevin Desai <kevindesai777@github.com>
2023-02-09 18:39:12 -0500

    design changes

--- app/js/main.js
+++ app/js/main.js
@@ -2,19 +2,19 @@
 //import json from '../collection.json' assert {type: 'json'};
 
 var json = [
-   {
-       "type": "FOTW",
-       "values": ["Japan", "Slovenia", "Sweden", "Czech Republic ", "Switzerland ", "Netherlands ", "United Kingdom ", "Hungary", "United Arab Emirates", "Austria", "Taiwan", "Romania", "Spain", "Andorra", "IN PROG - Moldova", "Gibraltar", "Sri Lanka", "India", "Liechtenstein", "Northern Ireland", "Estonia", "Finland", "Australia", "European Union", "Indonesia ", "Croatia ", "Germany ", "Hong Kong", "England", "China", "Canada", "Bulgaria", "Cambodia", "Malaysia", "Kosovo", "Albania", "Montenegro", "Italy", "Poland", "Ukraine", "Laos", "Vatican City (Arranged)", "Azores", "Norway", "Bosnia", "Chile", "Cyprus", "Greece", "Netherlands", "Philippines", "Argentina", "South Korea", "French Guyana (Arranged)", "Aland Island", "ASEAN", "Turkey (Arranged)", "Cocos Island", "Thailand (Arranged)", "Costa Rica", "Ireland", "Dominican Republic (Arranged)", "Palau", "Portugal", "Azerbaijan", "USA", "Macau (Arranged)", "Iceland", "Belgium", "Latvia", "Kazakhstan (Arranged)", "Israel (Arranged)", "Benin"]
-   },
-   {
-       "type": "MOTW",
-       "values": ["Austria", "Costa Rica", "Canada", "UK", "Hong Kong", "Italy", "Finland", "Norway", "Spain", "Malaysia", "Japan", "Germany", "Czech Republic", "Taiwan", "IN PROG - China", "England", "Croatia", "Thailand", "UAE", "Sovereign Military Order Of Malta", "Poland"]
+    {
+        "type": "FOTW",
+        "values": ["Japan", "Slovenia", "Sweden", "Czech Republic ", "Switzerland ", "Netherlands ", "United Kingdom ", "Hungary", "United Arab Emirates", "Austria", "Taiwan", "Romania", "Spain", "Andorra", "IN PROG - Moldova", "Gibraltar", "Sri Lanka", "India", "Liechtenstein", "Northern Ireland", "Estonia", "Finland", "Australia", "European Union", "Indonesia ", "Croatia ", "Germany ", "Hong Kong", "England", "China", "Canada", "Bulgaria", "Cambodia", "Malaysia", "Kosovo", "Albania", "Montenegro", "Italy", "Poland", "Ukraine", "Laos", "Vatican City (Arranged)", "Azores", "Norway", "Bosnia", "Chile", "Cyprus", "Greece", "Netherlands", "Philippines", "Argentina", "South Korea", "French Guyana (Arranged)", "Aland Island", "ASEAN", "Turkey (Arranged)", "Cocos Island", "Thailand (Arranged)", "Costa Rica", "Ireland", "Dominican Republic (Arranged)", "Palau", "Portugal", "Azerbaijan", "USA", "Macau (Arranged)", "Iceland", "Belgium", "Latvia", "Kazakhstan (Arranged)", "Israel (Arranged)", "Benin"]
+    },
+    {
+        "type": "MOTW",
+        "values": ["Austria", "Costa Rica", "Canada", "UK", "Hong Kong", "Italy", "Finland", "Norway", "Spain", "Malaysia", "Japan", "Germany", "Czech Republic", "Taiwan", "IN PROG - China", "England", "Croatia", "Thailand", "UAE", "Sovereign Military Order Of Malta", "Poland"]
 
-   },
-   {
-       "type": "Loupaper",
-       "values": ["Georgia", "Alabama"]
-   }
+    },
+    {
+        "type": "Loupaper",
+        "values": ["Georgia", "Alabama"]
+    }
 ]
 
 console.log(json)
@@ -32,36 +32,42 @@ console.log(json)
 //       });
 //       list.appendChild(fragList);
 
-document.getElementById("searchInput").onkeyup = function() {search()}
+document.getElementById("searchInput").onkeyup = function () { search() }
 search()
 
 function search() {
-   var input;
-   var filter;
-   input = document.getElementById("searchInput").value;
-   var searchList = document.getElementById("searchResult");
-   searchList.innerHTML = "";
-   var fragList = document.createDocumentFragment();
+    var input;
+    var filter;
+    input = document.getElementById("searchInput").value;
+    var searchList = document.getElementById("searchResult");
+    searchList.innerHTML = "";
+    var fragList = document.createDocumentFragment();
 
-   json.forEach(function (item) {
-      var values = item.values
-      var filteredRes = values.filter(
-          item => item.toLowerCase().includes(input.toLowerCase())
-      );
-      //console.log(filteredRes)
-      if (filteredRes.length !== 0) {
-         var li = document.createElement('li');
-         li.className = "list-group-item";
-         li.textContent = item.type;
-         var placeList = filteredRes.join(", ")
-         var ul = document.createElement("ul")
-         ul.textContent = placeList
-         li.appendChild(ul)
-         fragList.appendChild(li);
-         searchList.appendChild(fragList)
-      }
+    json.forEach(function (item) {
+        var values = item.values
+        var filteredRes = values.filter(
+            item => item.toLowerCase().includes(input.toLowerCase())
+        );
+        //console.log(filteredRes)
+        if (filteredRes.length !== 0) {
+            var topDiv = document.createElement('div');
+            topDiv.className = "card";
+            topDiv.style.textAlign = "center"
+            var headerDiv = document.createElement('div');
+            headerDiv.className = "card-header";
+            headerDiv.textContent = item.type;
+            headerDiv.style.fontSize = '18px';
+            topDiv.appendChild(headerDiv)
+            var placeList = filteredRes.join(", ")
+            var contentDiv = document.createElement("p")
+            contentDiv.textContent = placeList
+            contentDiv.className = "lead"
+            headerDiv.appendChild(contentDiv)
+            fragList.appendChild(topDiv);
+            searchList.appendChild(fragList)
+        }
 
-   });
-   //list.appendChild(fragList);
+    });
+    //list.appendChild(fragList);
 
 }
